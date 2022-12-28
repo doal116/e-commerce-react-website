5fe69e32d3cccea7d3e7070c6ff842c9e24ca83d
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2022-12-28 17:40:46 +0200

    feat: farmers & best selling section

--- src/components/bestSelling/bestSelling.js
+++ src/components/bestSelling/bestSelling.js
@@ -1,7 +1,26 @@
 import './bestSelling.css';
+import productIm from './naturalProduct.jpg'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+const CommonOptionSec = ({ nameOfSecion, categories, buttonMoreInfo }) => {
+    return (
+        <div className="CommonOptionSec">
+            <div className='title'>
+                <span>{nameOfSecion}</span>
+            </div>
+            <div className="categories">
+                {
+                    categories.map(elem => <a href={`www.google.com`}>{elem}</a>)
+                }
+            </div>
+            <button>
+                <span>{buttonMoreInfo}</span>
+                <FontAwesomeIcon className='icon' icon={faChevronRight} />
+            </button>
+        </div>
+    )
+}
 const BannerSubFocus = ({ banner }) => {
     return (
         banner.map(elem =>
@@ -21,37 +40,49 @@ const BannerSubFocus = ({ banner }) => {
 const CategoryMenu = ({ categories }) => {
     return (
         <div className="CategoryMenu">
-            <div className="CategorySec">
-                <div className='title'>
-                    <span>Category menu</span>
-                </div>
-
-                <div className="categories">
-                    {
-                        categories.map(elem => <a href={`www.google.com`}>{elem}</a>)
-                    }
-                </div>
-                <button>
-                    <span>More categories</span>
-                    <FontAwesomeIcon className='icon' icon={faChevronRight} />
-                </button>
-            </div>
-            <BannerSubFocus banner={[{ subText: "Banner subfocus", heading: "Space for heading", path: "/receipies" }, { subText: "Banner subfocus", heading: "Space for heading", path: "/receipies" }]} />
+            <CommonOptionSec categories={categories} nameOfSecion={"Category menu"} buttonMoreInfo={"More categories"} />
+            <BannerSubFocus banner={[{ subText: "Banner subfocus", heading: "Space for heading", path: "/receipies" },
+            { subText: "Banner subfocus", heading: "Space for heading", path: "/receipies" },
+            { subText: "Banner subfocus", heading: "Space for heading", path: "/receipies" }]} />
         </div>
 
     );
 }
-const BestSellingProducts = () => {
+const ProductBestSellingSection = ({ product }) => {
+    return (
+        product.map(elem => <div className='ProductSect'>
+            <img src={productIm} alt={"product from mother nature"} />
+            <div className='titleDescription'>
+                <span className='productTitle'>{elem.title}</span>
+                <span className='productDescrip'>{elem.description}</span>
+            </div>
+            <div className='buyingSection'>
+                <div className='price'>
+                    <span>{elem.price} USD</span>
+                </div>
+                <div className='btn-buy'><span>BUY NOW</span></div>
+            </div>
+        </div>
+        )
+    )
+}
+const BestSellingProducts = ({ categories }) => {
     return (
         <div className="BestSellingProducts">
-
+            <CommonOptionSec categories={categories} nameOfSecion={"Best selling products"} buttonMoreInfo={"More products"} />
+            <ProductBestSellingSection
+                product={[{ title: "Butter", description: "very tasty you wanna have more", price: 1.48 },
+                { title: "Butter", description: "very tasty you wanna have more", price: 1.48 },
+                { title: "Butter", description: "very tasty you wanna have more", price: 1.48 },
+                { title: "Butter", description: "very tasty you wanna have more", price: 1.48 }]}
+            />
         </div>
     );
 }
-const BestFromFarmers = () => {
+const BestFromFarmers = ({categories}) => {
     return (
         <div className="BestFromFarmers">
-
+            <CommonOptionSec categories={categories} nameOfSecion={"Best from Farmers"} buttonMoreInfo={"More products"} />
         </div>
     );
 }
@@ -76,8 +107,12 @@ class BestSelling extends React.Component {
                 <CategoryMenu
                     categories={["Bakery", "Fruits and Vegetables",
                         "Meat and fish", "Drinks", "Kitchen"]} />
-                <BestSellingProducts />
-                <BestFromFarmers />
+                <BestSellingProducts
+                    categories={["Kitchen", "Meat and Fish",
+                        "Special nutrition", "Pharmacy", "Kitchen"]} />
+                <BestFromFarmers
+                    categories={["Carrots", "Tommatoes",
+                        "Potatoes", "Chicken", "Pork"]} />
                 <Testimonials />
                 <SectionHeadlines />
             </div>
