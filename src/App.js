71e9a9e13f7af3ccaa42c632e9bf1dc65aea5f49
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2023-01-09 19:10:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,111 +8,67 @@ import HomePage from './components/HomePage/homePage';
 import Category from './components/category/category.js'
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import ProductDisplay from './components/productDisplay/productDisplay';
+
+const footerData = [{
+    title: "Get in touch", option: [
+        "About Us",
+        "Careers",
+        "Press Releases",
+        "Blog"
+    ]
+},
+{
+    title: "Connections", option: [
+        "Facebook",
+        "Twitter",
+        "Instagram",
+        "Youtube"
+    ]
+},
+{
+    title: "Earnings", option: [
+        "Become an Affiliate",
+        "Advertise your product",
+        "Sell on Market"
+    ]
+},
+{
+    title: "Account", option: [
+        "Your account",
+        "Returns Centre",
+        "100% purchase protection",
+        "Chat with us",
+        "Help"
+    ]
+}];
+const footerTags = [
+    "Beans", "carrots",
+    "apple", "garlic",
+    "mushrooms", "tomatoes",
+    "Chilli peppers", "Broccoli",
+    "Cherries", "Meat", "Seo tag"
+    , "Bananas", "Grapes", "watermelons",
+    "meat", "fish", "fresh food",
+    "seo Tag", "broccoli"
+]
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<>
-                <Header />
-                <HomePage />
-                <Footer sec1={[{
-                        title: "Get in touch",
-                        option: [
-                            "About Us",
-                            "Careers",
-                            "Press Releases",
-                            "Blog"
-                        ]
-
-                    },
-                    {
-                        title: "Connections",
-                        option: [
-                            "Facebook",
-                            "Twitter",
-                            "Instagram",
-                            "Youtube"
-                        ]
-                    },
-                    {
-                        title: "Earnings",
-                        option: [
-                            "Become an Affiliate",
-                            "Advertise your product",
-                            "Sell on Market"
-                        ]
-                    },
-                    {
-                        title: "Account",
-                        option: [
-                            "Your account",
-                            "Returns Centre",
-                            "100% purchase protection",
-                            "Chat with us",
-                            "Help"
-                        ]
-                    }
-                    ]} Tags={[
-                        "Beans", "carrots",
-                        "apple", "garlic",
-                        "mushrooms", "tomatoes",
-                        "Chilli peppers", "Broccoli",
-                        "Cherries", "Meat", "Seo tag"
-                        , "Bananas", "Grapes", "watermelons",
-                        "meat", "fish", "fresh food",
-                        "seo Tag", "broccoli"
-                    ]}/>
+                    <Header />
+                    <HomePage />
+                    <Footer sec1={footerData} Tags={footerTags} />
                 </>} />
                 <Route path="/category" element={<>
                     <Header />
                     <Category />
-                    <Footer sec1={[{
-                        title: "Get in touch",
-                        option: [
-                            "About Us",
-                            "Careers",
-                            "Press Releases",
-                            "Blog"
-                        ]
-
-                    },
-                    {
-                        title: "Connections",
-                        option: [
-                            "Facebook",
-                            "Twitter",
-                            "Instagram",
-                            "Youtube"
-                        ]
-                    },
-                    {
-                        title: "Earnings",
-                        option: [
-                            "Become an Affiliate",
-                            "Advertise your product",
-                            "Sell on Market"
-                        ]
-                    },
-                    {
-                        title: "Account",
-                        option: [
-                            "Your account",
-                            "Returns Centre",
-                            "100% purchase protection",
-                            "Chat with us",
-                            "Help"
-                        ]
-                    }
-                    ]} Tags={[
-                        "Beans", "carrots",
-                        "apple", "garlic",
-                        "mushrooms", "tomatoes",
-                        "Chilli peppers", "Broccoli",
-                        "Cherries", "Meat", "Seo tag"
-                        , "Bananas", "Grapes", "watermelons",
-                        "meat", "fish", "fresh food",
-                        "seo Tag", "broccoli"
-                    ]} /></>} />
+                    <Footer sec1={footerData} Tags={footerTags} /></>} />
+                <Route path="/product" element={<>
+                    <Header />
+                    <ProductDisplay />
+                    <Footer sec1={footerData} Tags={footerTags} /></>} />
             </Routes>
         </BrowserRouter>
     );
